cd21705c3742a04f4931c71d42200fdf3eec5d22
JulesDevine <jules.desmet@student.howest.be>
2025-09-18 19:52:26 +0200

    learing about components in angular

--- Experiments/1_First time installing/my-first-angular-app/src/app/app.component.ts
+++ Experiments/1_First time installing/my-first-angular-app/src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+  template: `
+    <app-header />
+    <app-home />
+  `,
+
+
+
 })
 export class AppComponent {
-  title = 'my-first-angular-app';
+  title = 'index page';
 }
