425c698910d75a17e0c32b4b4f86a9fd571c4e95
JulesDevine <jules.desmet@student.howest.be>
2025-10-13 12:50:47 +0200

    added routing to different pages and got rid of the standard template

--- Experiments/blog/src/app/app.routes.ts
+++ Experiments/blog/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { TodoPageComponent } from './todo-page/todo-page.component';
+import { PostsPageComponent } from './posts-page/posts-page.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+{
+  path: '',
+  component: HomePageComponent,
+},
+{
+  path: 'about',
+  component: AboutPageComponent,
+},
+{
+  path: 'todo',
+  component: TodoPageComponent,
+},
+{
+  path: 'posts',
+  component: PostsPageComponent,
+}
+];
